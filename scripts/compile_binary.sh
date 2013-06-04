14149e66200a809f61fe317110ed241d08d3f340
David Henderson <david.henderson@triggeredmessaging.com>
2013-06-04 02:55:35 -0700

    Working on Ubuntu64

--- scripts/compile_binary.sh
+++ scripts/compile_binary.sh
@@ -6,7 +6,7 @@ if [[ $OSTYPE = darwin* ]]; then
   echo "crazy stuff not needed on darwin"
 else
   # Had to mess around here - libfontconfig was not in /usr/lib. Had to also ensure that openssl was statically linked
-  flock ./Release/linker.lock g++ -shared -pthread -rdynamic -m64  -Wl,-soname=chimera.node -o Release/obj.target/chimera.node -Wl,--start-group Release/obj.target/chimera/src/top.o Release/obj.target/chimera/src/cookiejar.o Release/obj.target/chimera/src/chimera.o Release/obj.target/chimera/src/browser.o -Wl,--end-group  ../qt_compiled/lib/libQtXml.a ../qt_compiled/lib/libQtNetwork.a ../qt_compiled/lib/libQtWebKit.a ../qt_compiled/lib/libwebcore.a ../qt_compiled/lib/libjscore.a -Wl,--whole-archive ../qt_compiled/lib/libQtCore.a ../qt_compiled/lib/libQtGui.a  -Wl,--no-whole-archive -Wl, -Bsymbolic /usr/lib/x86_64-linux-gnu/libfontconfig.so -Bstatic ../openssl/libcrypto.a
+  flock ./Release/linker.lock g++ -shared -pthread -rdynamic -m64  -Wl,-soname=chimera.node -o Release/obj.target/chimera.node -Wl,--start-group Release/obj.target/chimera/src/top.o Release/obj.target/chimera/src/cookiejar.o Release/obj.target/chimera/src/chimera.o Release/obj.target/chimera/src/browser.o -Wl,--end-group  ../qt_compiled/lib/libQtXml.a ../qt_compiled/lib/libQtNetwork.a ../qt_compiled/lib/libQtWebKit.a ../qt_compiled/lib/libwebcore.a ../qt_compiled/lib/libjscore.a -Wl,--whole-archive ../qt_compiled/lib/libQtCore.a ../qt_compiled/lib/libQtGui.a  -Wl,--no-whole-archive -Wl,-Bsymbolic /usr/lib/x86_64-linux-gnu/libfontconfig.so -Bstatic -L../openssl -lssl -lcrypto
   make
 fi
 cp Release/chimera.node ../lib
--- scripts/compile_qt.sh
+++ scripts/compile_qt.sh
@@ -1,33 +1,24 @@
-#!/bin/bash
-
-
-# I had more luck using the version of qt/preconfig.sh
-# (clone phantomjs, copy the qt directory into node-chimera/qt)
-# Once compile is complete, copy from qt/lib to qt_compiled/lib and the same for qt/include
-
-
-
+#!/usr/bin/env bash
+ 
 cd qt
-
 COMPILE_JOBS=4
-
+ 
 QT_CFG=''
 QT_CFG+=' -opensource'          # Use the open-source license
 QT_CFG+=' -confirm-license'     # Silently acknowledge the license confirmation
 QT_CFG+=' -v'                   # Makes it easier to see what header dependencies are missing
 QT_CFG+=' -static'
-
+ 
 if [[ $OSTYPE = darwin* ]]; then
-    QT_CFG+=' -arch x86_64'
+    QT_CFG+=' -arch x86'
+    QT_CFG+=' -cocoa'           # Cocoa only, ignore Carbon
     QT_CFG+=' -no-dwarf2'
-    QT_CFG+=' -openssl'
 else
     QT_CFG+=' -system-freetype' # Freetype for text rendering
     QT_CFG+=' -fontconfig'      # Fontconfig for better font matching
     QT_CFG+=' -qpa'             # X11-less with QPA (aka Lighthouse)
-    QT_CFG+=' -openssl-linked'
 fi
-
+ 
 QT_CFG+=' -release'             # Build only for release (no debugging support)
 QT_CFG+=' -fast'                # Accelerate Makefiles generation
 QT_CFG+=' -nomake demos'        # Don't build with the demos
@@ -35,17 +26,15 @@ QT_CFG+=' -nomake docs'         # Don't generate the documentatio
 QT_CFG+=' -nomake examples'     # Don't build any examples
 QT_CFG+=' -nomake translations' # Ignore the translations
 QT_CFG+=' -nomake tools'        # Don't built the tools
-
+ 
 QT_CFG+=' -no-exceptions'       # Don't use C++ exception
 QT_CFG+=' -no-stl'              # No need for STL compatibility
-QT_CFG+=' -javascript-jit'
-QT_CFG+=' -webkit'
-
+ 
 # Irrelevant Qt features
 QT_CFG+=' -no-libmng'
 QT_CFG+=' -no-libtiff'
 QT_CFG+=' -no-icu'
-
+ 
 # Unnecessary Qt modules
 QT_CFG+=' -no-declarative'
 QT_CFG+=' -no-multimedia'
@@ -57,15 +46,14 @@ QT_CFG+=' -no-script'
 QT_CFG+=' -no-scripttools'
 QT_CFG+=' -no-svg'
 QT_CFG+=' -no-xmlpatterns'
-
+ 
 # Unnecessary Qt features
 QT_CFG+=' -D QT_NO_GRAPHICSVIEW'
 QT_CFG+=' -D QT_NO_GRAPHICSEFFECT'
-
-# Sets the default graphics system to the raster enginecd qt
-
+ 
+# Sets the default graphics system to the raster engine
 QT_CFG+=' -graphicssystem raster'
-
+ 
 # Unix
 QT_CFG+=' -no-dbus'             # Disable D-Bus feature
 QT_CFG+=' -no-glib'             # No need for Glib integration
@@ -75,22 +63,22 @@ QT_CFG+=' -no-cups'             # Disable CUPs support
 QT_CFG+=' -no-sm'
 QT_CFG+=' -no-xinerama'
 QT_CFG+=' -no-xkb'
-
+ 
 # Use the bundled libraries, vs system-installed
 QT_CFG+=' -qt-libjpeg'
 QT_CFG+=' -qt-libpng'
 QT_CFG+=' -qt-zlib'
-
-
+ 
+# Explicitly compile with SSL support, so build will fail if headers are missing
+QT_CFG+=' -openssl-linked'
+ 
 # Useless styles
 QT_CFG+=' -D QT_NO_STYLESHEET'
 QT_CFG+=' -D QT_NO_STYLE_CDE'
 QT_CFG+=' -D QT_NO_STYLE_CLEANLOOKS'
 QT_CFG+=' -D QT_NO_STYLE_MOTIF'
 QT_CFG+=' -D QT_NO_STYLE_PLASTIQUE'
-QT_CFG+=' -L../openssl -lssl -lcrypto'
-
-
+ 
 until [ -z "$1" ]; do
     case $1 in
         "--qt-config")
@@ -114,33 +102,32 @@ until [ -z "$1" ]; do
             exit 1;;
     esac
 done
-
-
+ 
+ 
 # For parallelizing the bootstrapping process, e.g. qmake and friends.
 export MAKEFLAGS=-j$COMPILE_JOBS
 
-#export OPENSSL_LIBS='-L../openssl -lssl -lcrypto'
-
-#./configure -prefix ../qt_compiled $QT_CFG
+if [[ $OSTYPE != darwin* ]]; then
+  export OPENSSL_LIBS='-L../openssl -lssl -lcrypto'
+fi
+./configure -prefix ../qt_compiled $QT_CFG
 make -j$COMPILE_JOBS install
-
+ 
 cd src/3rdparty/webkit/Source/WebCore
 make -j$COMPILE_JOBS
 cd ../../../../..
-
+ 
 cd src/3rdparty/webkit/Source/JavaScriptCore
 make -j$COMPILE_JOBS
 cd ../../../../..
-
-# Extra step to ensure the static libraries are found
+ 
 cp -rp src/3rdparty/webkit/Source/JavaScriptCore/release/* ../qt_compiled/lib/
 cp -rp src/3rdparty/webkit/Source/WebCore/release/* ../qt_compiled/lib/
-
+ 
 cat include/QtGui/QtGui | grep -v -e 'qs60' -e 'qvfbhdr' -e 'qwsembedwidget' > ../qt_compiled/include/QtGui/QtGui
-
+ 
 rm -rf ../qt_compiled/include/QtScript
 rm -rf ../qt_compiled/include/QtWebKit
 cd ../qt_compiled/include
-ln -s ./include/QtScript .
-ln -s ./include/QtWebKit .
-
+ln -s ../../qt/include/QtScript .
+ln -s ../../qt/include/QtWebKit .
